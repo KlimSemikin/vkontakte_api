99d2472d5def7c63a5a0fb1e240625d9e89265bc
KlimSem <klimsemikin@gmail.com>
2025-05-21 15:54:33 +0300

    replace oauth2 `param` with `bearer`

--- lib/vkontakte_api/api.rb
+++ lib/vkontakte_api/api.rb
@@ -33,7 +33,7 @@ module VkontakteApi
         token = options.delete(:token)
         
         Faraday.new(url, VkontakteApi.faraday_options) do |builder|
-          builder.request :oauth2, token, token_type: 'param' unless token.nil?
+          builder.request :oauth2, token, token_type: 'bearer' unless token.nil?
           builder.request :multipart
           builder.request :url_encoded
           builder.request :retry, VkontakteApi.max_retries
